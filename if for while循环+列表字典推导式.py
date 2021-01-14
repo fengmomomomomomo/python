50181a57ccd81a52d64900c43463932c2c7ee5bf
fengmomomomo <lirong98@163.com>
2021-01-14 11:20:40 +0800

    练习if for while 字典及列表推导式

--- if for while循环+列表字典推导式.py
+++ if for while循环+列表字典推导式.py
@@ -7,23 +7,73 @@
 # else:
 #     print('x与123不相等')
 
+# # for基本语句
+# for i in sengxiao_name:
+#     print(i)
+#
+# for j in range(1,13): # 13是到13前的一个数字，即1到12
+#     print(j)
+
+# # while基本语句——break
+# num = int(input('输入一个值'))
+# while True:
+#     print('a')
+#     num = num + 1
+#     if num > 10:
+#         break
+
+# # while基本语句——continue
+# num = int(input('输入一个值'))
+# while True:
+#     num = num + 1
+#     if num == 10:
+#         continue        #continue是跳过下面的程序，执行下一次的循环
+#     print(num)
+#
+#     if num > 12:
+#         break
+
+# # 定义字典
+# dict1 = {'x': 1, 'y': 2}  # 用冒号赋予变量的值
+# print(type(dict1))
+# dict1['z'] = 1
+# print(dict1)
+# print(dict1)
+
+# # 列表推导式——计算偶数的平方
+# list_a = []
+# for i in range(13):
+#     if i % 2 == 0:
+#         list_a.append(i*i)
+# print(list_a)
+#
+# #  等同于如下列表推导式
+# list_b = [i*i for i in range(13) if i % 2 == 0]
+# print(list_b)
+
+
+
 
 
 # # 申明生肖和星座的变量
 sengxiao_name = ('猴鸡狗猪鼠牛虎兔龙蛇马羊')
 zodiac_name = (u'摩羯座', u'水瓶座', u'双鱼座', u'白羊座', u'金牛座', u'双子座',
-           u'巨蟹座', u'狮子座', u'处女座', u'天秤座', u'天蝎座', u'射手座')
+                 u'巨蟹座', u'狮子座', u'处女座', u'天秤座', u'天蝎座', u'射手座')
 zodiac_days = ((1, 20), (2, 19), (3, 21), (4, 21), (5, 21), (6, 22),
               (7, 23), (8, 23), (9, 23), (10, 23), (11, 23), (12, 23))
 
 # # 建立字典
-dict_sengxiao = {}
-for i in sengxiao_name:
-    dict_sengxiao[i] = 0
+# dict_sengxiao = {}
+# for i in sengxiao_name:
+#     dict_sengxiao[i] = 0
+#
+# dict_xingzuo = {}
+# for i in zodiac_name:
+#     dict_xingzuo[i] = 0
+# # 等同于如下字典推导式
+dict_sengxiao = {i: 0 for i in sengxiao_name}
 
-dict_xingzuo = {}
-for i in zodiac_name:
-    dict_xingzuo[i] = 0
+dict_xingzuo = {i: 0 for i in zodiac_name}
 
 while True:     # 使得用户可以重复输入年月日
     # # 注意这里要记得缩进，包括在大的while循环里
@@ -68,43 +118,4 @@ while True:     # 使得用户可以重复输入年月日
 
 
 
-# # for基本语句
-# for i in sengxiao_name:
-#     print(i)
-#
-# for j in range(1,13): # 13是到13前的一个数字，即1到12
-#     print(j)
-
-# # while基本语句——break
-# num = int(input('输入一个值'))
-# while True:
-#     print('a')
-#     num = num + 1
-#     if num > 10:
-#         break
-
-# # while基本语句——continue
-# num = int(input('输入一个值'))
-# while True:
-#     num = num + 1
-#     if num == 10:
-#         continue        #continue是跳过下面的程序，执行下一次的循环
-#     print(num)
-#
-#     if num > 12:
-#         break
-
-
-#
-
-
-
-
-## 定义字典
-# dict1 = {}
-# print(type(dict1))
-# dict1['x'] = 1
-# print(dict1)
-# dict1['z','y'] = (2,3)
-# print(dict1)
 
